ca1d8dbaa14fd58e754b14c059988df4702b1db1
Minervus00 <fayouedraogo@protonmail.com>
2024-05-07 23:38:14 +0000

    fixing conflicts

--- test.py
+++ test.py
@@ -8,29 +8,12 @@ class TestCliFeature(unittest.TestCase):
         dc.DB = "cli_test.db"
         dc.create_tables()
 
-<<<<<<< HEAD
-    def test_user_insertion(self):
-=======
     def test_a_user_insertion(self):
->>>>>>> 25ce4bbda1e13006211d268f547428353c3bd959
         user1 = ("Dembe", "dembe@black.list", "ddddd", 1)
         user2 = ("Shelby", "shelby@peaky.com", "1a2b3c", 0)
         self.assertTrue(dc.insert_user(*user1))
         self.assertTrue(dc.insert_user(*user2))
 
-<<<<<<< HEAD
-    def test_insert_task(self):
-        tsk1 = ("test et integration",
-                "terminer le projet de test et integration",
-                0, 2)
-        tsk2 = ("push up",
-                "en faire 30 chaque jour", 0, 2)
-        self.assertTrue(dc.insert_tasks(*tsk1))
-        self.assertTrue(dc.insert_tasks(*tsk2))
-
-    def test_delete_task(self):
-        self.assertTrue(dc.delete_task(2, 2))
-=======
     def test_b_insert_task(self):
         tsk1 = ("test et integration",
                 "terminer le projet de test et integration",
@@ -42,7 +25,6 @@ class TestCliFeature(unittest.TestCase):
                 "en faire 30 chaque jour", 0, 2)
         self.assertTrue(dc.insert_tasks(*tsk2))
         self.assertTrue(dc.delete_task(1, 2))
->>>>>>> 25ce4bbda1e13006211d268f547428353c3bd959
 
     def test_d_set_task_done(self):
         self.assertTrue(dc.set_task_done(2, 2))
